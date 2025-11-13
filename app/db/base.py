b15f80347f6c54c6e44d78a64901ada1a2da9b63
Parsa Rahbari <prahbari82@gmail.com>
2025-11-13 22:25:35 +0330

    feat: Implement DB session and ORM models

---
+++ app/db/base.py
@@ -0,0 +1,5 @@
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
+
+class Base(DeclarativeBase):
+    """Base for all ORM models."""
+    pass
---
+++ app/db/session.py
@@ -0,0 +1,28 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db_session():
+    """Dependency to get a new DB session."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
